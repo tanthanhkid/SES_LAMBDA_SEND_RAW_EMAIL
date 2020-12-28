4c634d0ef713c94588c13f97ebbe3ade0c10b095
tanthanhkid <tanthanhkid@gmail.com>
2020-12-28 22:46:01 +0700

    fix code

--- index.py
+++ index.py
@@ -1,48 +1,100 @@
-import boto3
-import logging
 import os
-import json
+import boto3
 from botocore.exceptions import ClientError
-
-sesv2 = boto3.client("sesv2")
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 def lambda_handler(event, context):
-
-    global log_level
-    log_level = str(os.environ.get("LOG_LEVEL")).upper()
-    if log_level not in [
-                            "DEBUG", "INFO",
-                            "WARNING", "ERROR",
-                            "CRITICAL"
-                        ]:
-        log_level = "ERROR"
-    logging.getLogger().setLevel(log_level)
-
-    logging.info(event)
-
-
-    for record in event['Records']:
-      try:
-        ses_event = json.loads(json.loads(record['body'])['Message'])
-
-        logging.info(ses_event)
-
-        # If event is a Bounce, handle it
-        if (ses_event["notificationType"] == "Bounce" and ses_event["bounce"]["bounceType"] == "Permanent" ):
-            for recipient in ses_event["bounce"]["bouncedRecipients"]:
-                sesv2.put_suppressed_destination(
-                  EmailAddress = recipient["emailAddress"],
-                  Reason = "BOUNCE"
-                )
-                # CUSTOMER TODO - Update customer system with Bounce status
-
-        # If event is a spam complaint
-        elif (ses_event["notificationType"] == "Complaint"):
-            for recipient in ses_event["complaint"]["complainedRecipients"]:
-                sesv2.put_suppressed_destination(
-                  EmailAddress = recipient["emailAddress"],
-                  Reason = "COMPLAINT"
-                )
-                # CUSTOMER TODO - Update customer system with Complaint status
-      except Exception as e:
-          logging.error("Received Error while processing SQS record: %s", e)
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "tan thanh <tanthanhkid@gmail.com>"
+    
+    # Replace recipient@example.com with a "To" address. If your account 
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = "trantanthanh95@live.com"
+    
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the 
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    # CONFIGURATION_SET = "ConfigSet"
+    
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "ap-southeast-1"
+    
+    # The subject line for the email.
+    SUBJECT = "Customer service contact info"
+    
+    # The full path to the file that will be attached to the email.
+    ATTACHMENT = "att_fasd.txt"
+    
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = "Hello,\r\nPlease see the attached file for a list of customers to contact."
+    
+    # The HTML body of the email.
+    BODY_HTML = """\
+    <html>
+    <head></head>
+    <body>
+    <h1>Hello!</h1>
+    <p>Please see the attached file for a list of customers to contact.</p>
+    </body>
+    </html>
+    """
+    
+    # The character encoding for the email.
+    CHARSET = "utf-8"
+    
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+    
+    # Create a multipart/mixed parent container.
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = SUBJECT 
+    msg['From'] = SENDER 
+    msg['To'] = RECIPIENT
+    
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+    
+    # Encode the text and HTML content and set the character encoding. This step is
+    # necessary if you're sending a message with characters outside the ASCII range.
+    textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+    htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+    
+    # Add the text and HTML parts to the child container.
+    msg_body.attach(textpart)
+    msg_body.attach(htmlpart)
+    
+    # Define the attachment part and encode it using MIMEApplication.
+    att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+    
+    # Add a header to tell the email client to treat this part as an attachment,
+    # and to give the attachment a name.
+    att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+    
+    # Attach the multipart/alternative child container to the multipart/mixed
+    # parent container.
+    msg.attach(msg_body)
+    
+    # Add the attachment to the parent container.
+    msg.attach(att)
+    #print(msg)
+    try:
+        #Provide the contents of the email.
+        response = client.send_raw_email(
+            Source=SENDER,
+            Destinations=[
+                RECIPIENT
+            ],
+            RawMessage={
+                'Data':msg.as_string(),
+            }
+            #ConfigurationSetName=CONFIGURATION_SET
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
